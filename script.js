481b31ba1a5ed8314f55a5bab8e621593d6f9fa2
Manuel Alejandro Londoño Cadavid <malondonoc@gmail.com>
2023-10-16 14:36:30 -0500

    Added first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var current_year = new Date().getFullYear();
+    var years = document.getElementById('years').value;
+    var interest = principal * years * rate / 100;
+    var year = current_year + parseInt(years);
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
+
+    if(principal <= 0){
+        alert("Please enter a positive number");
+        document.getElelmentById("principal").focus();
+        
+    }else {
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    }
+    
+}
+
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+        
